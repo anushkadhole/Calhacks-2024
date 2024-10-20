05d281f0fc9c518cdf3a01c92cc1c72cbafb340e
Anushka Dhole <anushka.dhole@sjsu.edu>
2024-10-20 10:30:17 -0700

    Adding App.js to git

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+// src/App.js
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Tasks from './pages/Tasks';
 import Navbar from './components/Navbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#3f51b5' }, // Blue
+    secondary: { main: '#f50057' }, // Pink 1
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tasks" element={<Tasks />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
 export default App;
-
